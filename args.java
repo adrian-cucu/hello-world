492f271f5801edcdfb3db57b8503634fb424a280
Adrian <adriancucu96@yahoo.com>
2017-07-21 01:42:26 +0300

    nicer

--- args.java
+++ args.java
@@ -1,11 +1,7 @@
 
-class args 
-{
-	public static void main (String[] args)
-	{
-		for (int i = 0; i < args.length; ++ i)
-		{
-			System.out.println (args [i]);
-		}
+class args{
+	public static void main (String[] args){
+		for (String arg : args)
+			System.out.println (arg);
 	}	
 }
